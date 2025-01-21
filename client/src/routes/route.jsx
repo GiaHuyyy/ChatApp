7ff6b3c17629e487ed67b05b66dc72348d05570b
Gia Huy <tovugiahuy@gmail.com>
2025-01-21 22:00:35 +0700

    Làm login bằng QR và bằng phone, mật khẩu

--- client/src/routes/route.jsx
+++ client/src/routes/route.jsx
@@ -1,42 +1,66 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import RegisterPage from "../pages/RegisterPage";
-import CheckEmailPage from "../pages/CheckEmailPage";
-import CheckPasswordPage from "../pages/CheckPasswordPage";
+// import RegisterPage from "../pages/RegisterPage";
+// import CheckPhonePage from "../pages/LoginWithPhonePage";
+// import CheckPasswordPage from "../pages/CheckPasswordPage";
+// import LoginWithQR from "../pages/LoginWithQR";
 import Home from "../pages/Home";
 import MessagePage from "../components/MessagePage";
+import AuthLayout from "../layout/AuthLayout";
 
-const router = createBrowserRouter(
-    [
-        {
-            path: "/",
-            element: <App />,
-            children: [
-                {
-                    path: "/register",
-                    element: <RegisterPage />,
-                },
-                {
-                    path: "/email",
-                    element: <CheckEmailPage />,
-                },
-                {
-                    path: "/password",
-                    element: <CheckPasswordPage />,
-                },
-                {
-                    path: "",
-                    element: <Home />,
-                    children: [
-                        {
-                            path: ":userId",
-                            element: <MessagePage />,
-                        }
-                    ]
-                },
-            ]
-        }
-    ]
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/auth",
+        element: <AuthLayout />,
+      },
+      // {
+      //   path: "/register",
+      //   element: (
+      //     <AuthLayout>
+      //       <RegisterPage />
+      //     </AuthLayout>
+      //   ),
+      // },
+      // {
+      //   path: "/qr",
+      //   element: (
+      //     <AuthLayout>
+      //       <LoginWithQR />
+      //     </AuthLayout>
+      //   ),
+      // },
+      // {
+      //   path: "/phone",
+      //   element: (
+      //     <AuthLayout>
+      //       <CheckPhonePage />
+      //     </AuthLayout>
+      //   ),
+      // },
+      // {
+      //   path: "/password",
+      //   element: (
+      //     <AuthLayout>
+      //       <CheckPasswordPage />
+      //     </AuthLayout>
+      //   ),
+      // },
+      {
+        path: "",
+        element: <Home />,
+        children: [
+          {
+            path: ":userId",
+            element: <MessagePage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 export default router;
