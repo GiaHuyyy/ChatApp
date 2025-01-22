af95f3d62c6b07649dace6b9100d4d2ca64a84b6
Gia Huy <tovugiahuy@gmail.com>
2025-01-22 21:52:25 +0700

    Làm chức năng login bằng phone và password

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,27 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 export default function Home() {
+  useEffect(() => {
+    const fetchUseDetails = async () => {
+      try {
+        const URL = `${import.meta.env.VITE_APP_BACKEND_URL}/api/user-details`;
+        const response = await axios.get(URL, { withCredentials: true });
+        console.log(response.data);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    };
+    fetchUseDetails();
+  }, []);
   return (
     <div>
       Home
-
       {/* Message component */}
       <section>
         <Outlet />
       </section>
-      </div>
-  )
+    </div>
+  );
 }
--- client/src/pages/LoginWithPhonePage.jsx
+++ client/src/pages/LoginWithPhonePage.jsx
@@ -1,5 +1,9 @@
-import { faLock, faMobile } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faEyeSlash, faLock, faMobile } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
 export default function CheckPhonePage() {
   const contries = [
@@ -16,9 +20,46 @@ export default function CheckPhonePage() {
       code: "+81",
     },
   ];
+  const [data, setData] = useState({
+    phone: "",
+    password: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleOnChange = (e) => {
+    setData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    try {
+      const URL = `${import.meta.env.VITE_APP_BACKEND_URL}/api/login`;
+      const response = await axios.post(URL, data, { withCredentials: true });
+      toast.success(response.data.message);
+      if (response.data.success) {
+        setData({
+          phone: "",
+          password: "",
+        });
+      }
+      navigate("/", { replace: true });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center">
-      <form className="w-[310px]">
+      <form className="w-[310px]" onSubmit={handleLogin}>
         <div className="border-b border-[#f0f0f0] flex items-center mb-[18px] py-[5px]">
           <FontAwesomeIcon icon={faMobile} width={8.5} />
           <select name="country" id="country" className="w-[70px] p-1 text-sm">
@@ -28,12 +69,33 @@ export default function CheckPhonePage() {
               </option>
             ))}
           </select>
-          <input type="tel" name="phone" id="phone" placeholder="Số điện thoại" className="flex-1 text-sm ml-3" />
+          <input
+            type="tel"
+            name="phone"
+            id="phone"
+            placeholder="Số điện thoại"
+            className="flex-1 text-sm ml-3"
+            value={data.phone}
+            onChange={handleOnChange}
+            required
+          />
         </div>
 
         <div className="border-b border-[#f0f0f0] flex items-center mb-[18px] py-[5px]">
           <FontAwesomeIcon icon={faLock} width={8.5} />
-          <input type="password" name="password" id="password" placeholder="Mật khẩu" className="flex-1 text-sm ml-3" />
+          <input
+            type={showPassword ? "text" : "password"}
+            name="password"
+            id="password"
+            placeholder="Mật khẩu"
+            className="flex-1 text-sm ml-3"
+            value={data.password}
+            onChange={handleOnChange}
+            required
+          />
+          <span className="cursor-pointer ml-2" onClick={handleShowPassword}>
+            <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} width={10} />
+          </span>
         </div>
 
         <button className="h-[44px] px-5 bg-[#0190f3] text-white w-full font-medium">Đăng nhập với mật khẩu</button>
